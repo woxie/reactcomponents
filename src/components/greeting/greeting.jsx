46863cef4da4891022c163e0bdf71b9b89e813d2
wox <maganainurreta@gmail.com>
2023-08-22 13:59:35 -0600

    ok

---
+++ src/components/greeting/greeting.jsx
@@ -0,0 +1,8 @@
+export default function greeting(){
+    return(
+        <div>
+            <h1>Hola</h1>
+            <h2>Texto</h2>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// importamos una función que esta exportada como default
+import greeting from './components/greeting/greeting';
+
+/*
+React.StrictMode: Componente de React que se utiliza en el desarrollo
+para detectar y advertir sobre posibles problemas en una aplicación.
+
+*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +17,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
